c8b9c086ecdb41b55dc902b41aac732d1e26f890
william.duan <william.duan@spotterio.com.cn>
2023-03-15 15:33:46 +0800

    v0.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,62 @@
-import { window } from 'vscode'
+import path from 'node:path'
+import type { ExtensionContext } from 'vscode'
+import { Uri, window, workspace } from 'vscode'
+import { parse } from '@babel/parser'
+import { version } from '../package.json'
+import { registerAnnotations } from './registerAnnotation'
+import { log } from './log'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+export async function activate(ext: ExtensionContext) {
+  log.appendLine(`⚪️ spotter-i18n-hint for VS Code v${version}\n`)
+
+  window.showInformationMessage(`⚪️ spotter-i18n-hint for VS Code v${version}\n`)
+
+  const projectPath = workspace.workspaceFolders?.[0].uri.fsPath
+  if (!projectPath) {
+    log.appendLine('➖ No active workspace found, spotter-i18n-hint is disabled')
+    return
+  }
+
+  const config = workspace.getConfiguration('spotter')
+  const disabled = config.get<boolean>('disable', false)
+  if (disabled) {
+    log.appendLine('➖ Disabled by configuration')
+    return
+  }
+
+  const cwd = projectPath
+
+  try {
+    // client/node_modules/@spotter/i18n-sdk/lib/trans/index.d.ts
+    const i18nDtsFilePath = config.get<string>('i18nDtsFilePath') || 'client/node_modules/@spotter/i18n-sdk/lib/trans/index.d.ts'
+    const dtsFilePath = path.join(cwd, i18nDtsFilePath)
+    const dtsFileUri = Uri.file(dtsFilePath)
+    // 解析dts文件，获取所有的key和value，然后拼接成一个对象
+    const readData = await workspace.fs.readFile(dtsFileUri)
+    const dtsFileStr = Buffer.from(readData).toString('utf8')
+    const ast = parse(dtsFileStr, {
+      sourceType: 'module',
+      plugins: ['typescript'],
+    })
+    const obj: Record<string, string> = {}
+    // dts 的 ast生成的结构，debug出来的
+    const i18nData = (ast.program.body[0] as any).declarations[0].id.typeAnnotation.typeAnnotation.members
+    i18nData.forEach((item: any) => {
+      const key = item.key?.value
+      const value = item.typeAnnotation.typeAnnotation.literal?.value
+      if (!key || !value)
+        log.appendLine(`➖ key or value is undefined, key: ${key}, value: ${value}`)
+      else
+        obj[key] = value
+    })
+
+    // 将obj的key按照长度排序，然后拼接成正则表达式，其中特殊符号需要转义
+    const regEx = new RegExp(Object.keys(obj).sort((a, b) => b.length - a.length).map(item => item.replace(/([.*+?^=!:${}()|[\]/\\])/g, '\\$1')).join('|'), 'g')
+    registerAnnotations(cwd, ext, obj, regEx)
+  }
+  catch (e: any) {
+    log.appendLine(String(e.stack ?? e))
+  }
 }
 
-export function deactivate() {
-
-}
+export function deactivate() { }
---
+++ src/registerAnnotation.ts
@@ -0,0 +1,107 @@
+import path from 'node:path'
+import type { DecorationOptions, ExtensionContext } from 'vscode'
+import { DecorationRangeBehavior, MarkdownString, Range, window, workspace } from 'vscode'
+
+export async function registerAnnotations(cwd: string,
+  ext: ExtensionContext,
+  obj: Record<string, string>,
+  regEx: RegExp) {
+  const UnderlineDecoration = window.createTextEditorDecorationType({
+    textDecoration: 'none; border-bottom: 1px dashed currentColor',
+    rangeBehavior: DecorationRangeBehavior.ClosedClosed,
+  })
+  const NoneDecoration = window.createTextEditorDecorationType({
+    textDecoration: 'none',
+    rangeBehavior: DecorationRangeBehavior.ClosedClosed,
+  })
+
+  const colorDecoration = window.createTextEditorDecorationType({
+    before: {
+      width: '0.9em',
+      height: '0.9em',
+      contentText: ' ',
+      border: '1px solid',
+      margin: 'auto 0.2em auto 0;vertical-align: middle;border-radius:50%;',
+    },
+    dark: {
+      before: {
+        borderColor: '#eeeeee50',
+      },
+    },
+    light: {
+      before: {
+        borderColor: '#00000050',
+      },
+    },
+  })
+
+  async function updateAnnotation(editor = window.activeTextEditor) {
+    try {
+      function reset() {
+        editor?.setDecorations(UnderlineDecoration, [])
+        editor?.setDecorations(NoneDecoration, [])
+        editor?.setDecorations(colorDecoration, [])
+      }
+      const doc = editor?.document
+
+      if (!doc)
+        return reset()
+
+      const id = doc.uri.fsPath
+      // 当前打开的不属于项目内
+      if (!isSubdir(cwd, id))
+        return reset()
+
+      const code = doc.getText()
+      if (!code)
+        return reset()
+
+      const text = editor.document.getText()
+      // 匹配text
+      let match
+      const i18nKeys: DecorationOptions[] = []
+      // eslint-disable-next-line no-cond-assign
+      while (match = regEx.exec(text)) {
+        const startPos = editor.document.positionAt(match.index)
+        const endPos = editor.document.positionAt(match.index + match[0].length)
+        const decoration = {
+          range: new Range(startPos, endPos),
+          hoverMessage: new MarkdownString(`**${obj[match[0]]}**`),
+        }
+        i18nKeys.push(decoration)
+      }
+
+      editor.setDecorations(NoneDecoration, [])
+      editor.setDecorations(UnderlineDecoration, i18nKeys)
+    }
+    catch (error) { }
+  }
+
+  const throttledUpdateAnnotation = throttle(updateAnnotation, 200)
+  window.onDidChangeActiveTextEditor(updateAnnotation)
+  workspace.onDidChangeTextDocument((e) => {
+    if (e.document === window.activeTextEditor?.document)
+      throttledUpdateAnnotation()
+  })
+  await updateAnnotation()
+}
+export function isSubdir(parent: string, child: string) {
+  const relative = path.relative(parent, child)
+  return relative && !relative.startsWith('..') && !path.isAbsolute(relative)
+}
+export function throttle<T extends ((...args: any) => any)>(func: T, timeFrame: number): T {
+  let lastTime = 0
+  let timer: any
+  return function (...args) {
+    const now = Date.now()
+    clearTimeout(timer)
+    if (now - lastTime >= timeFrame) {
+      lastTime = now
+      return func(...args)
+    }
+    else {
+      timer = setTimeout(func, timeFrame, ...args)
+    }
+  } as T
+}
+
