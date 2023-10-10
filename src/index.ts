40ffe2de93b81b4770c3316ed271e4fb055fbf02
kitiho <duanjipeng0328@gmail.com>
2023-10-10 15:20:02 +0800

    feat: 配置

--- src/index.ts
+++ src/index.ts
@@ -20,19 +20,38 @@ export async function activate() {
     return
   }
 
+  const i18n_api_key = config.get<string>('i18n_api_key', '')
+  const i18n_operator_id = config.get<string>('i18n_operator_id', '')
+  const i18n_project_id = config.get<string>('i18n_project_id', '')
+  const i18n_namespace_id = config.get<string>('i18n_namespace_id', '')
+  if (!i18n_api_key || !i18n_operator_id || !i18n_project_id || !i18n_namespace_id) {
+    log.appendLine('➖ i18n_api_key or i18n_operator_id or i18n_project_id or i18n_namespace_id is empty')
+    return
+  }
+
   const cwd = projectPath
 
   try {
-    const obj: Record<'zh' | 'en', Record<string, Record<string, string>>> = {
+    const obj: Record<'zh' | 'en', Record<string, string>> = {
       en: {},
       zh: {},
     }
-    const zhData: any = await getI18nSource('zh')
-    const enData: any = await getI18nSource('en')
+    const zhData: any = await getI18nSource('zh', {
+      i18n_api_key,
+      i18n_operator_id,
+      i18n_project_id,
+      i18n_namespace_id,
+    })
+    const enData: any = await getI18nSource('en', {
+      i18n_api_key,
+      i18n_operator_id,
+      i18n_project_id,
+      i18n_namespace_id,
+    })
     obj.zh = zhData
     obj.en = enData
     const allKeys = Object.keys(obj.zh)
-    // // 将obj的key按照长度排序，然后拼接成正则表达式，其中特殊符号需要转义
+    // 将obj的key按照长度排序，然后拼接成正则表达式，其中特殊符号需要转义
     const regEx = new RegExp(
       allKeys
         .sort((a, b) => b.length - a.length)
